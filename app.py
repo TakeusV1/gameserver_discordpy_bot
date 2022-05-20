db5a09168ce4a229291229f267dd8fa8639c1fb0
Takeus <68923554+pomme2terree@users.noreply.github.com>
2022-05-20 17:58:32 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from discord.ext import tasks, commands
 from mcstatus import JavaServer
 
 ## Config
-token = 'xxxxxxxxxxxxxxx' # Token BOT
+token = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx' # Token BOT
 server_ip = 'xx.xx.xx.xx' # Addresse IP 
 server_port = 'xxxxx' # Port
 bot_prefix = '!!' # Préfixe
@@ -18,21 +18,35 @@ async def on_ready():
         loop.start()
     print('Ready')
 
-@tasks.loop(seconds=2)
+@tasks.loop(seconds=5)
 async def loop():
-    server = JavaServer.lookup(str(server_ip+':'+server_port))
-    status = server.status()
-    await bot.change_presence(activity=discord.Game(name="{}/{} Joueurs".format(status.players.online,status.players.max)))
+    try:
+        server = JavaServer.lookup(str(server_ip+':'+server_port))
+        status = server.status()
+        if status.players.online == 0:
+            await bot.change_presence(activity=discord.Game(name="{}/{} Joueurs".format(status.players.online,status.players.max)),status=discord.Status.idle)
+        else:
+            await bot.change_presence(activity=discord.Game(name="{}/{} Joueurs".format(status.players.online,status.players.max)),status=discord.Status.online)
+
+    except:
+        await bot.change_presence(activity=discord.Game(name="Serveur Hors-Ligne"),status=discord.Status.dnd)
 
 @bot.command()
 async def info(ctx):
-    server = JavaServer.lookup(str(server_ip+':'+server_port))
-    status = server.status()
-    await ctx.channel.send("__**Joueurs**__: {}/{}\n__**Latence**__: {} *ms*".format(status.players.online,status.players.max,server.ping()))
+    try:
+        server = JavaServer.lookup(str(server_ip+':'+server_port))
+        status = server.status()
+        await ctx.channel.send("__**Joueurs**__: {}/{}\n__**Latence**__: {} *ms*".format(status.players.online,status.players.max,server.ping()))
+    except:
+        await ctx.channel.send("Serveur Hors-Ligne")
 
 @bot.command()
 async def cmd(ctx):
     await ctx.channel.send(f"__**Liste des Commandes**__:\n {bot_prefix}info *(Affiche les informations du Serveur)*")
+
+@bot.command()
+async def ping(ctx):
+    await ctx.channel.send(f'Bot UP !\nLatence: {round(bot.latency, 1)}')
 
 bot.run(token)
 
